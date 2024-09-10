a8cd8635e5154578a04c6d60750b8b6bdee420d0
chikafx <kingsobi70@yahoo.com>
2024-09-10 10:33:11 +0100

    feat: 1st backend assignment done

---
+++ intro/class.py
@@ -0,0 +1,43 @@
+print("Hello world!")
+
+print (type(2))
+print (type(3.3))
+
+print (type('clara'))
+
+name= 'king'
+print (name)
+print (type(name))
+
+age, height = 23, 5.5
+print(age, height)
+print(age, height, name)
+
+status= "single"
+# this is called the concatinating string
+text= name + ' is ' + status
+
+print(text)
+
+# this is called an (f) string
+new_text= f"{name} is {status}"
+
+print(new_text)
+
+age= "29"
+status="married"
+zodiac_sign="leo"
+occupation="fullstack"
+internship= "univelcity"
+
+#this is called the {F} formatter
+bio= f"""
+
+my name is {name} i am currently {status}, i am {age}
+my zodiac sign is {zodiac_sign} and my occupation{occupation}
+and i am currently interning at {internship}
+
+"""
+
+print(bio)
+
---
+++ intro/string_assignment.py
@@ -0,0 +1,22 @@
+first_name= 'Kamso'
+last_name= 'Obi'
+age= '29 years'
+hobby = "playing drums"
+
+FullName=first_name + last_name
+
+print("Hello,", FullName)
+
+intro_message= f" hello, my name is {first_name} {last_name} and i am {age}"
+
+print(intro_message)
+
+
+hobby_message=f"""
+hello, my name ia {first_name} {last_name} and i am {age} and i live and reside in Lagos
+and my favorite hobby is {hobby}
+"""
+
+print(hobby_message)
+
+personal_info= first_name + last_name
